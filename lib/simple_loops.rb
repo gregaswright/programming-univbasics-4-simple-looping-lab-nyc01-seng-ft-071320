1e2261686690d59ed4d13268a26cb93e03bd1db5
Gregory A.S. Wright <geslim@gmail.com>
2020-06-25 19:15:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,3 +13,11 @@ def loop_message_n_times(message, integer)
     count += 1
   end
 end
+
+def output_array(array)
+  counter = 0
+  while counter < array.length do
+    puts array
+    counter += 1
+  end
+end
